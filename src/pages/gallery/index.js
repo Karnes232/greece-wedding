1c9b1162135d999072db223336507579d1c9639c
James Karnes <karnes.james@gmail.com>
2023-06-02 14:41:47 -0400

    setup gallery page

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,8 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../../components/layout"
 
-const index = () => {
-  return <Layout>Photo Gallery</Layout>
+import PhotoAlbum from "react-photo-album"
+
+import Lightbox from "yet-another-react-lightbox"
+import "yet-another-react-lightbox/styles.css"
+
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen"
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow"
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails"
+import Zoom from "yet-another-react-lightbox/plugins/zoom"
+import "yet-another-react-lightbox/plugins/thumbnails.css"
+import photos from "../../data/photos"
+
+const Index = () => {
+  const [index, setIndex] = useState(-1);
+  console.log(photos)
+  return (
+    <Layout>
+      <main className="">
+        <PhotoAlbum
+          photos={photos}
+          layout="rows"
+          targetRowHeight={150}
+          onClick={({ index }) => setIndex(index)}
+        />
+
+        <Lightbox
+          slides={photos}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          // enable optional lightbox plugins
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        />
+      </main>
+    </Layout>
+  )
 }
 
-export default index
+export default Index
