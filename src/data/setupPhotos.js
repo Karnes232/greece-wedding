b4f8e8b41822a94c504a752704d1894abdcdd235
James Karnes <karnes.james@gmail.com>
2023-06-03 18:53:35 -0400

    added more photos to gallery

---
+++ src/data/setupPhotos.js
@@ -0,0 +1,427 @@
+const setupPhotoList = [
+  {
+    src: "https://i.postimg.cc/vmYNfWYq/setup14.webp",
+    height: 1920,
+    width: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/vmYNfWYq/setup14.webp",
+        height: 1920,
+        width: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/90KBR2DY/setup14.webp",
+        width: 427,
+        height: 640,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/x1RsfcvK/setup23.webp",
+    height: 1920,
+    width: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/x1RsfcvK/setup23.webp",
+        height: 1920,
+        width: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/vTJW8vKP/setup23.webp",
+        width: 427,
+        height: 640,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Z5FrZpxc/setup1.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Z5FrZpxc/setup1.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/W3sYTsZD/setup1.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/J7vvvDx0/setup10.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/J7vvvDx0/setup10.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/yNynf2G7/setup10.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/CL8XghrN/setup11.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/CL8XghrN/setup11.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/hj4sNpFT/setup11.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/9QjSX6vj/setup12.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/9QjSX6vj/setup12.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/7hfVZhZL/setup12.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Z5FQWfSv/setup13.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Z5FQWfSv/setup13.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/50PgJL1G/setup13.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/bJdVFp21/setup15.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/bJdVFp21/setup15.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/654cs8Pj/setup15.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/g0gBDnrY/setup16.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/g0gBDnrY/setup16.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/7PW9Q3KJ/setup16.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/XJhHRmwm/setup17.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/XJhHRmwm/setup17.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/9Q3pcW5W/setup17.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Znd74HHR/setup18.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Znd74HHR/setup18.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/rpMNtQw3/setup18.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/mZz6YZ4s/setup19.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/mZz6YZ4s/setup19.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/FHjxR2N0/setup19.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/fRhx6Bmy/setup2.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/fRhx6Bmy/setup2.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/JnK6hS3p/setup2.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/MZC3dkZj/setup20.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/MZC3dkZj/setup20.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/R0dQ1K3F/setup20.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Ghv7fM9g/setup21.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Ghv7fM9g/setup21.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/qR7c1qnG/setup21.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Jzm6Cbkr/setup22.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Jzm6Cbkr/setup22.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/C5BGV2cB/setup22.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/VNkKdmg2/setup24.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/VNkKdmg2/setup24.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/c127csJQ/setup24.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/vm23nkST/setup25.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/vm23nkST/setup25.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/WbZGWQDx/setup25.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/RhHHYH0w/setup3.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/RhHHYH0w/setup3.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/hPNChLxR/setup3.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/Dm3X3wWH/setup4.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/Dm3X3wWH/setup4.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/JzJpXscD/setup4.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/V6Tt4RzY/setup5.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/V6Tt4RzY/setup5.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/WpWYcL13/setup5.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/wBqDBMWd/setup6.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/wBqDBMWd/setup6.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/RhFPJGX4/setup6.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/vBD9WjFq/setup7.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/vBD9WjFq/setup7.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/9FD8W8DB/setup7.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/k5TKRCZR/setup8.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/k5TKRCZR/setup8.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/QCyfQ5y5/setup8.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+  {
+    src: "https://i.postimg.cc/05bhVdph/setup9.webp",
+    width: 1920,
+    height: 1280,
+    srcSet: [
+      {
+        src: "https://i.postimg.cc/05bhVdph/setup9.webp",
+        width: 1920,
+        height: 1280,
+      },
+      {
+        src: "https://i.postimg.cc/LspN3mNy/setup9.webp",
+        width: 640,
+        height: 427,
+      },
+    ],
+  },
+]
