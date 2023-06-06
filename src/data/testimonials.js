8ce4843bcd9d6442e95efccbb94f6b74044097af
James Karnes <karnes.james@gmail.com>
2023-06-06 18:07:20 -0400

    updated reviews

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,20 +1,20 @@
 const testimonials = [
   {
     review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem reiciendis, tempora nesciunt, quam ad maxime eos cumque in vel similique aperiam quibusdam natus eum perferendis omnis eaque debitis qui optio quisquam quod molestias sunt expedita tenetur? Nobis illo sint aut?",
-    client: "LIBBY A.",
+      "Greece Weddings exceeded all our expectations! Their impeccable attention to detail, professional staff, and breathtaking venues made our special day truly magical. From start to finish, their team ensured a seamless and unforgettable experience. We highly recommend Greece Weddings for anyone seeking perfection and memories to cherish forever!",
+    client: "Betty",
     reviewLocation: "Google",
   },
   {
     review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem reiciendis, tempora nesciunt, quam ad maxime eos cumque in vel similique aperiam quibusdam natus eum perferendis omnis eaque debitis qui optio quisquam quod molestias sunt expedita tenetur? Nobis illo sint aut?",
-    client: "Johny B.",
+      "Choosing Greece Weddings for our event was the best decision we made! The team's creativity, passion, and impeccable execution brought our vision to life. From start to finish, they went above and beyond to ensure every detail was perfect. Our guests were in awe, and we couldn't be happier with the outcome. Greece Weddings truly made our dreams come true!",
+    client: "Richard",
     reviewLocation: "Weddings.com",
   },
   {
     review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem reiciendis, tempora nesciunt, quam ad maxime eos cumque in vel similique aperiam quibusdam natus eum perferendis omnis eaque debitis qui optio quisquam quod molestias sunt expedita tenetur? Nobis illo sint aut?",
-    client: "Jane Doe",
+      "Our experience with Greece Weddings was simply exceptional! Their team went above and beyond to ensure that every aspect of our wedding was perfect. From the stunning venue selection to the flawless organization, they left no stone unturned. The level of professionalism and genuine care they exhibited made us feel truly valued. We are forever grateful for the unforgettable memories they helped create.",
+    client: "Virginia",
     reviewLocation: "TripAdvisor",
   },
 ]
