ac1d58cf90fd50f526c06b2f1afb8beada73927e
James Karnes <karnes.james@gmail.com>
2023-06-10 13:55:00 -0400

    updated photos for photo session page

--- src/data/pages/photoSessionPhotos.js
+++ src/data/pages/photoSessionPhotos.js
@@ -1,10 +1,10 @@
 //Photos for Carousel
-const carouselPhoto1 = "https://i.postimg.cc/HLS47sgq/wedding26.webp"
-const carouselPhoto1Mobile = "https://i.postimg.cc/tgdNRtS4/wedding26.webp"
-const carouselPhoto2 = "https://i.postimg.cc/MHZVdWnw/wedding7.webp"
-const carouselPhoto2Mobile = "https://i.postimg.cc/brXNwLjB/wedding7.webp"
-const carouselPhoto3 = "https://i.postimg.cc/50gFHZnR/wedding4.webp"
-const carouselPhoto3Mobile = "https://i.postimg.cc/bY2ZfSj5/wedding4.webp"
+const carouselPhoto1 = "https://i.postimg.cc/5yVKsnqV/51.webp"
+const carouselPhoto1Mobile = "https://i.postimg.cc/nzRYD2R2/51.webp"
+const carouselPhoto2 = "https://i.postimg.cc/3NHF3Vzy/7.webp"
+const carouselPhoto2Mobile = "https://i.postimg.cc/BQjHdqjh/7.webp"
+const carouselPhoto3 = "https://i.postimg.cc/76SStXbq/14.webp"
+const carouselPhoto3Mobile = "https://i.postimg.cc/76BJWcw8/14.webp"
 
 export const carouselPhotos = [
   {
@@ -22,5 +22,5 @@ export const carouselPhotos = [
 ]
 
 export const serviceDescriptionPhoto =
-  "https://i.postimg.cc/bY2ZfSj5/wedding4.webp"
-export const serviceListPhoto = "https://i.postimg.cc/brXNwLjB/wedding7.webp"
+  "https://i.postimg.cc/GmV80NBv/26.webp"
+export const serviceListPhoto = "https://i.postimg.cc/VkrvFR7v/35.webp"
