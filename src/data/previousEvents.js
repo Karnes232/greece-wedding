c844d908ce15f753cacceb5aa3e4fcbaa487fa79
James Karnes <karnes.james@gmail.com>
2023-06-10 17:06:17 -0400

    adding previous events

--- src/data/previousEvents.js
+++ src/data/previousEvents.js
@@ -1,25 +1,25 @@
-import image from "../images/wedding.jpg"
-import image2 from "../images/wedding2.jpg"
-import image3 from "../images/wedding3.jpg"
+const image = "https://i.postimg.cc/Gm8rVN4s/conference.webp"
+const image2 = "https://i.postimg.cc/NfPBNYjc/business-Drinks.webp"
+const image3 = "https://i.postimg.cc/4xDZg5c2/business-Meet.webp"
 
 const previousEvents = [
   {
     image: image,
     eventTitle: "ALEXA + CONOR’S ROMANTIC JUNE WEDDING",
     description:
-      "A romantic wedding ceremony and reception with garden-inspired decor at the Bridge Building",
+      "An insightful gathering of industry leaders in Punta Cana fostering growth and teamwork",
   },
   {
     image: image2,
     eventTitle: "GAGE + BRIAN’S ROMANTIC GARDEN-INSPIRED WEDDING",
     description:
-      "A romantic and modern June wedding with neutral decor and bright flower arrangements at The Bell Tower",
+      "A lively evening of networking and conversation as professionals connect over drinks",
   },
   {
     image: image3,
     eventTitle: "MACKENZIE + JACK’S TROPICAL SUMMER WEDDING",
     description:
-      "A summer wedding with disco ball decor, tropical leaves, and pops of navy blue at The Cordelle",
+      "Colleagues unite for a productive conference in picturesque Punta Cana, fostering teamwork and growth",
   },
 ]
 
