960a1e9a92789ad249f01fbd4b227b49a6486848
James Karnes <karnes.james@gmail.com>
2023-06-11 17:45:32 -0400

    changed text color in textarea in contact page

--- src/components/ContactFormComponents/MoreInfo.js
+++ src/components/ContactFormComponents/MoreInfo.js
@@ -26,7 +26,7 @@ const MoreInfo = () => {
           id="additional"
           name="additional"
           rows="4"
-          className="block p-2.5 w-full text-sm text-gray-500 rounded-lg border border-gray-300 focus:ring-0 focus:border-black additionalInfo"
+          className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-0 focus:border-black additionalInfo"
           placeholder="Special Requests..."
         ></textarea>
       </div>
