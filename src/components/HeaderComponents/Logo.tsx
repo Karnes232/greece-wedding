0572d2411f0b7ebee5e2b5ff6742b54cf177b6b5
James Karnes <karnes.james@gmail.com>
2023-06-15 17:13:47 -0400

    adjusting logo size

--- src/components/HeaderComponents/Logo.tsx
+++ src/components/HeaderComponents/Logo.tsx
@@ -2,20 +2,25 @@ import React from "react"
 
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import logo from "../../images/greeceLogo.webp"
 const Logo = () => {
   return (
     <>
       <div className="flex justify-center items-center overflow-hidden">
         <Link to="/" className="no-underline" aria-label="Home">
           <div className="cursor-pointer flex items-center w-24 h-20 md:w-32 md:h-24 xl:w-64 xl:h-44">
-            <StaticImage
+            {/* <StaticImage
               src="../../images/greeceLogo.png"
               loading="eager"
               quality={100}
               formats={["auto", "webp", "avif"]}
               alt=""
-            />
+            /> */}
+            <img
+                src={logo}
+                className=""
+                alt=""
+              />
           </div>
         </Link>
       </div>
